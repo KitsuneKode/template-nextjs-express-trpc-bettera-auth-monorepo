7e7bf4f608de502ee29aa558852617b6a6242722
Manash Pratim Bhuyan <bhuyanmanash2002@gmail.com>
2025-09-28 18:05:02 +0530

    fix version issues

--- apps/server/src/middlewares/timing-middleware.ts
+++ apps/server/src/middlewares/timing-middleware.ts
@@ -1,24 +1,19 @@
-import config from '@/utils/config'
-import { logger } from '@/utils/logger'
 import type { NextFunction, Request, Response } from 'express'
+import { logger } from '@/utils/logger'
+import config from '@/utils/config'
 
-export const timingMiddleWare = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+export const timingMiddleWare = async (req: Request, res: Response, next: NextFunction) => {
   const start = Date.now()
 
   if (config.getConfig('nodeEnv') === 'development') {
     // artificial delay in dev 100-500ms
     const waitMs = Math.floor(Math.random() * 400) + 100
-    await new Promise((resolve) => setTimeout(resolve, waitMs))
+    setTimeout(() => next(), waitMs)
+  } else {
+    next()
   }
-
-  const result = await next()
-
-  const end = Date.now()
-  logger.info(`[AUTH] ${req.path} took ${end - start}ms to execute`)
-
-  return result
+  res.on('finish', () => {
+    const end = Date.now()
+    logger.info(`[AUTH] ${req.path} took ${end - start}ms to execute`)
+  })
 }
